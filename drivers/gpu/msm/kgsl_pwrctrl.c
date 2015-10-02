8c4c69b1bca511994592bb4a6b0c56984be6a215
apr20th311 <apr20th311@gmail.com>
2015-10-02 23:32:06 +0800

    sth fixed

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -191,8 +191,6 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 #ifdef CONFIG_CPU_FREQ_GOV_SLIM
         graphics_boost = pwr->active_pwrlevel;
 #endif
-}
-EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
 
 		/*
 		 * On some platforms, instability is caused on
